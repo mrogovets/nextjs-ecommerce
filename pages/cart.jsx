a9742248d822b5a8d6b6ca4dd977157a365ec068
Maksym Rohovets <mrohovets@gmail.com>
2021-10-24 21:14:00 +0300

    realized cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,5 +1,28 @@
+import Head from "next/head";
+import { useContext } from "react";
+import { DataContext } from "../store/GlobalState";
 const Cart = () => {
-  return <div>Cart</div>;
+  const { state, dispatch } = useContext(DataContext);
+  const { cart } = state;
+
+  if (cart.length === 0)
+    return (
+      <img
+        className="img-responsive w-100"
+        src="/empty_cart.jpg"
+        alt="Empty cart"
+      />
+    );
+
+  return (
+    <div>
+      <Head>
+        <title>Cart Page</title>
+      </Head>
+
+      <h1>Cart</h1>
+    </div>
+  );
 };
 
 export default Cart;

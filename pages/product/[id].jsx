548355f6d5524c0eb3e79c7e23ba0d95a6379761
Maksym Rohovets <mrohovets@gmail.com>
2021-10-24 17:24:59 +0300

    2:56:04

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -5,6 +5,12 @@ import { getData } from "../../utils/fetchData";
 const DetailProduct = (props) => {
   const [product] = useState(props.product);
   const [tab, setTab] = useState(0);
+
+  const isActive = (index) => {
+    if (tab === index) return "active";
+    return "";
+  };
+
   return (
     <div className="row detail_page">
       <Head>
@@ -25,13 +31,33 @@ const DetailProduct = (props) => {
               key={index}
               src={img.url}
               alt={img.url}
-              className="img-thumbnail rounded"
+              className={`img-thumbnail rounded ${isActive(index)}`}
               style={{ height: "80px", width: "20%" }}
               onClick={() => setTab(index)}></img>
           ))}
         </div>
       </div>
-      <div className="col-md-6"></div>
+      <div className="col-md-6 mt-3">
+        <h2 className="text-uppercase">{product.title}</h2>
+        <h5 className="text-danger">${product.price}</h5>
+        <div className="row mx-0 d-flex justify-content-between">
+          {product.inStock > 0 ? (
+            <h6 className="text-danger">In stock: {product.inStock}</h6>
+          ) : (
+            <h6 className="text-danger">Out Stock</h6>
+          )}
+          <h6 className="text-danger">Sold: {product.sold}</h6>
+        </div>
+        <div className="my-2">{product.description}</div>
+        <div className="my-2">
+          {product.content}
+          {product.content}
+          {product.content}
+        </div>
+        <button type="button" className="btn btn-dark d-block my-3 px-5">
+          Buy
+        </button>
+      </div>
     </div>
   );
 };

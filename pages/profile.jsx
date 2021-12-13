14b1048bbce3c2e322e278285c6c4eceaf2afccf
mrogovets <mrogovets@ukr.net>
2021-12-13 22:54:56 +0200

    05:19:07

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { DataContext } from "../store/GlobalState";
+
+import valid from "../utils/valid";
+import { patchData } from "../utils/fetchData";
 
 const Profile = () => {
   const initialState = {
@@ -13,7 +16,36 @@ const Profile = () => {
   const { avatar, name, password, cf_password } = data;
 
   const { state, dispatch } = useContext(DataContext);
-  const { auth } = state;
+  const { auth, notify } = state;
+
+  useEffect(() => {
+    if (auth.user) setData({ ...data, name: auth.user.name });
+  }, [auth.user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+    dispatch({ type: "NOTIFY", payload: {} });
+  };
+
+  const handleUpdateProfile = (e) => {
+    e.preventDefault();
+    if (password) {
+      const errMsg = valid(name, auth.user.email, password, cf_password);
+      if (errMsg)
+        return dispatch({ type: "NOTIFY", payload: { error: errMsg } });
+      updatePassword();
+    }
+  };
+
+  const updatePassword = () => {
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+    patchData("user/resetPassword", { password }, auth.token).then((res) => {
+      if (res.err)
+        return dispatch({ type: "NOTIFY", payload: { error: res.msg } });
+      return dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+    });
+  };
 
   if (!auth.user) return null;
 
@@ -45,6 +77,7 @@ const Profile = () => {
               value={name}
               className="form-control"
               placeholder="Your name"
+              onChange={handleChange}
             />
           </div>
 
@@ -55,33 +88,40 @@ const Profile = () => {
               name="email"
               defaultValue={auth.user.email}
               className="form-control"
-              disabled="true"
+              disabled={true}
             />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">New Password</label>
             <input
-              type="text"
+              type="password"
               name="password"
               value={password}
               className="form-control"
               placeholder="Your new password"
+              onChange={handleChange}
             />
           </div>
 
           <div className="form-group">
             <label htmlFor="cf_password">Confirm New Password</label>
             <input
-              type="text"
+              type="password"
               name="cf_password"
               value={cf_password}
               className="form-control"
               placeholder="Confirm New password"
+              onChange={handleChange}
             />
           </div>
 
-          <button className="btn btn-info">Update</button>
+          <button
+            className="btn btn-info"
+            disabled={notify.loading}
+            onClick={handleUpdateProfile}>
+            Update
+          </button>
         </div>
 
         <div className="col-md-8">
